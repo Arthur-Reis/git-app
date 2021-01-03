c525fc4acddb61c490f1fb85ae4fbca33e686f23
Arthur Reis <rarthureis@gmail.com>
2021-01-03 16:16:17 -0300

    feat: config Prettier e editorConfig

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
 };
