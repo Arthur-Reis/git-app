2b2c95d493fcaf4796caf98df7d1c1643ef0b3ed
Arthur Reis <rarthureis@gmail.com>
2021-02-09 11:07:18 -0300

    postando projeto

---
+++ readme.md
@@ -0,0 +1,17 @@
+## Setup
+Renomeie **.env.example** para **.env** e adicione seu [github TOKEN](https://github.com/settings/tokens) e github EMAIL</p>  
+
+Agora inicie o app com
+```
+Yarn android
+Yarn start
+```
+
+### Notes
+- [ ] Não está pronto para a produção
+
+### Referência Técnica 
+
+- [React Navigation](https://reactnavigation.org/)</p>
+- [Axios](https://github.com/axios/axios)</p>
+- [Humps](https://www.npmjs.com/package/humps)</p>
--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import HomeScreen from './pages/homeScreen'
 
-import { FormProvider } from './contexts/form'
 const Stack = createStackNavigator()
 
 function App() {
   return (
-    <FormProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </FormProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
